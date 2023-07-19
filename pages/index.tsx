b7faba87f96c076d81a7be4ebe5158b3bd10edad
rest <vlad.shkerdin@relsys.eu>
2023-07-20 02:50:17 +0300

    build test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,11 @@ import { i18n } from 'Core/i18n';
 
 const some = i18n.t('base:success');
 
-const GoogleAuthView:React.FC = () => {
-    return (
-        <>
-            {some}
-        </>
-    );
-};
+const GoogleAuthView:React.FC = () => (
+    <>
+        {some}
+    </>
+);
 
 export default GoogleAuthView;
 
